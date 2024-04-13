4ed1d2a50ebfb418c41f38edf5676a847574399c
ReiRev <reirev2913@gmail.com>
2024-04-13 09:56:47 +0000

    fix duplicated creation bug

--- src/stories/stone/SlateStone.tsx
+++ src/stories/stone/SlateStone.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { drawLightReflexion } from "./stoneUtils";
 
 interface SlateStoneProps {
@@ -13,31 +13,45 @@ export const SlateStone = ({
   simple = true,
 }: SlateStoneProps) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+
   useEffect(() => {
     if (canvasRef.current) {
-      const context = canvasRef.current.getContext("2d");
-      if (!context) return;
-      if (simple) {
-        const center = size / 2;
-        const x = center;
-        const y = center;
-        const colorIn = "#666";
-        const colorOut = "#000";
-        const radiusIn = 0.01;
-        const radiusOut = 0.75;
-
-        drawLightReflexion(
-          context,
-          x,
-          y,
-          radius,
-          colorIn,
-          colorOut,
-          radiusIn,
-          radiusOut,
-        );
+      const renderContext = canvasRef.current.getContext("2d");
+      if (!renderContext) {
+        console.error("Failed to get 2D rendering context.");
+        return;
       }
+      setContext(renderContext);
     }
-  });
+  }, [canvasRef]);
+
+  useEffect(() => {
+    if (context) {
+      // clear context
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    }
+    if (context && simple) {
+      const center = size / 2;
+      const x = center;
+      const y = center;
+      const colorIn = "#666";
+      const colorOut = "#000";
+      const radiusIn = 0.01;
+      const radiusOut = 0.75;
+
+      drawLightReflexion(
+        context,
+        x,
+        y,
+        radius,
+        colorIn,
+        colorOut,
+        radiusIn,
+        radiusOut,
+      );
+    }
+  }, [context, size, radius, simple]);
+
   return <canvas ref={canvasRef} />;
 };
